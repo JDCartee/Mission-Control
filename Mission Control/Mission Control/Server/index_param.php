0cd31affc69a66708e4323891c28f08936a955f0
Jeremy Cartee <jerm@cartee.net>
2014-06-23 19:38:41 -0400

    - Fixed missing echo in php code

--- Mission Control/Mission Control/Server/index_param.php
+++ Mission Control/Mission Control/Server/index_param.php
@@ -1,14 +1,14 @@
 <html>
 <body>
 <h1>Mission Control</h1>
-<p>Now lets kick off the python script...</p> 
+<p>Now lets kick off the python script...</p>
 <?php
 $output_pre=shell_exec('whoami');
-echo "<pre>The server user running the script is $output_pre</pre>";
+echo "The server user running the script is <pre>$output_pre</pre>";
 $key_param=$_GET["key"];
 echo "<pre>The key value you gave me is $key_param</pre>";
-$output=shell_exec("sudo /usr/bin/python /home/pi/Documents/GPIO/blink_param.py $key_param 2>&1");
-<p>Here is your script output...</p>
+$output=shell_exec("sudo /usr/bin/python /home/pi/Documents/GPIO/blink_param.py$
+echo "<p>Here is your script output...</p>";
 echo "<pre>$output</pre>";
 ?>
 </body>
